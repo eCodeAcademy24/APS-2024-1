0ed5fe0b3370fc9287f1055b3b368b7b475fd260
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-05-15 22:17:19 +0200

    Class 1 and 2

---
+++ Class 1 & 2 (SLL & SLLNode)/untitled/src/zadaca103/Zadaca103.java
@@ -0,0 +1,304 @@
+package zadaca103;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        // Construct an empty SLL
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int length() {
+        int ret;
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret = 1;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret++;
+            }
+            return ret;
+        } else
+            return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp + "->";
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += tmp + "->";
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+
+    public void insertBefore(E o, SLLNode<E> before) {
+
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == before) {
+                this.insertFirst(o);
+                return;
+            }
+            //ako first!=before
+            while (tmp.succ != before)
+                tmp = tmp.succ;
+            if (tmp.succ == before) {
+                SLLNode<E> ins = new SLLNode<E>(o, before);
+                tmp.succ = ins;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            SLLNode<E> ins = new SLLNode<E>(o, null);
+            tmp.succ = ins;
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            first = first.succ;
+            return tmp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == node) {
+                return this.deleteFirst();
+            }
+            while (tmp.succ != node && tmp.succ.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == node) {
+                tmp.succ = tmp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.element != o && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element == o) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return first;
+    }
+
+    public Iterator<E> iterator() {
+        // Return an iterator that visits all elements of this list, in left-to-right order.
+        return new LRIterator<E>();
+    }
+
+    // //////////Inner class ////////////
+
+    private class LRIterator<E> implements Iterator<E> {
+
+        private SLLNode<E> place, curr;
+
+        private LRIterator() {
+            place = (SLLNode<E>) first;
+            curr = null;
+        }
+
+        public boolean hasNext() {
+            return (place != null);
+        }
+
+        public E next() {
+            if (place == null)
+                throw new NoSuchElementException();
+            E nextElem = place.element;
+            curr = place;
+            place = place.succ;
+            return nextElem;
+        }
+
+        public void remove() {
+            //Not implemented
+        }
+    }
+
+    public void mirror() {
+        if (first != null) {
+            //m=nextsucc, p=tmp,q=next
+            SLLNode<E> tmp = first;
+            SLLNode<E> newsucc = null;
+            SLLNode<E> next;
+
+            while (tmp != null) {
+                next = tmp.succ;
+                tmp.succ = newsucc;
+                newsucc = tmp;
+                tmp = next;
+            }
+            first = newsucc;
+        }
+
+    }
+
+    public void merge(SLL<E> in) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = in.getFirst();
+        } else {
+            first = in.getFirst();
+        }
+    }
+}
+
+//Дадена е линеарно поврзана листа. Да се напише функција која во листата ќе го
+//пронајде последниот јазол со информација x и ќе го префрли на чело на листата. Како
+//резултат функцијата да ја враќа позицијата (броено од почетокот на листата) на која
+//бил пронајден бараниот јазол или -1 ако таков јазол не бил пронајден во листата.
+//Input
+// 8
+// 3 4 7 6 4 7 3 2
+// 7
+//Output
+// 7 3 4 7 6 4 3 2
+// позиција 6
+
+
+public class Zadaca103 {
+    public static int zadaca103(SLL<Integer> lista, int x) {
+        SLLNode<Integer> tmp = lista.find(x);
+        if (tmp == null || tmp.element != x) {
+            return -1;
+        }
+
+        tmp = lista.getFirst();
+        SLLNode<Integer> pamtiTmp = null;
+
+        int counter = 1;
+        int pamtiCounter = 0;
+
+        while (tmp != null) {
+            if (tmp.element == x) {
+                pamtiTmp = tmp;
+                pamtiCounter = counter;
+            }
+
+            ++counter;
+            tmp = tmp.succ;
+        }
+
+//        if (pamtiTmp == null /* pamtiCounter == 0 */ ) {
+//            return -1;
+//        }
+
+        lista.insertFirst(x);
+        lista.delete(pamtiTmp);
+        System.out.println(lista);
+        return pamtiCounter;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 8
+        // 3 4 7 6 4 7 3 2
+        // 7
+        String s;
+        s = br.readLine();
+        int n = Integer.parseInt(s);
+
+        SLL<Integer> lista = new SLL<Integer>();
+
+        s = br.readLine();
+        String[] parts = s.split(" ");
+
+        for (int i = 0; i < n; i++) {
+            lista.insertLast(Integer.parseInt(parts[i]));
+        }
+
+        s = br.readLine();
+        int x = Integer.parseInt(s);
+
+
+        System.out.println(zadaca103(lista, x));
+
+    }
+}
