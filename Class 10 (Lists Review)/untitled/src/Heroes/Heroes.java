5df4faf36cfca90c2acfc2aeca9603afe8bdcd64
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-06-10 20:59:25 +0200

    Reviews

--- Class 10 (Lists Review)/untitled/src/Heroes/Heroes.java
+++ Class 10 (Lists Review)/untitled/src/Heroes/Heroes.java
@@ -1,6 +1,51 @@
-package ChangeList;
+package Heroes;
 
 import java.util.Scanner;
+
+class Card {
+    private int id;
+    private int power;
+    private int numAttacks;
+
+    public Card(int id, int power, int numAttacks) {
+        this.id = id;
+        this.power = power;
+        this.numAttacks = numAttacks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getNumAttacks() {
+        return numAttacks;
+    }
+
+    public void setNumAttacks(int numAttacks) {
+        this.numAttacks = numAttacks;
+    }
+
+    public int damage() {
+        return power * numAttacks;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+}
 
 class SLLNode<E> {
     protected E element;
@@ -17,12 +62,10 @@ class SLLNode<E> {
     }
 }
 
-
 class SLL<E> {
     private SLLNode<E> first;
 
     public SLL() {
-        // Construct an empty SLL
         this.first = null;
     }
 
@@ -50,10 +93,10 @@ class SLL<E> {
         String ret = new String();
         if (first != null) {
             SLLNode<E> tmp = first;
-            ret += tmp + "->";
+            ret += tmp;
             while (tmp.succ != null) {
                 tmp = tmp.succ;
-                ret += tmp + "->";
+                ret += " " + tmp;
             }
         } else
             ret = "Prazna lista!!!";
@@ -75,14 +118,12 @@ class SLL<E> {
     }
 
     public void insertBefore(E o, SLLNode<E> before) {
-
         if (first != null) {
             SLLNode<E> tmp = first;
             if (first == before) {
                 this.insertFirst(o);
                 return;
             }
-            //ako first!=before
             while (tmp.succ != before)
                 tmp = tmp.succ;
             if (tmp.succ == before) {
@@ -138,7 +179,6 @@ class SLL<E> {
             System.out.println("Listata e prazna");
             return null;
         }
-
     }
 
     public SLLNode<E> getFirst() {
@@ -162,60 +202,101 @@ class SLL<E> {
     }
 }
 
-// Дадена е еднострано/двострано поврзана листа која содржи едноцифрени броеви и меѓу нив е и -1.
-// Да се модифицира листата на тој начин што ќе се формираат броеви од тие цифри се додека не се стигне до бројот -1.
-// Кога ќе се стигне до тој број се прекинува се брише -1 и се продолжува во формирање на броеви од наредните цифри (постапката се повторува).
-//Влез:
-//14
-//1 2 3 -1 5 -1 6 7 4 8 9 -1 9 8
-//Излез:
-//123 5 67489 98
+//Input
+//92 98 48
+//18 48 15
+//43 20 5
+//51 87 76
+//16 35 96
+//24 41 75
+//12 8 62
+//98 53 61
+//77 47 94
+//81 31 20
+//56 21 37
+//27 3 65
+//Output
+//92 18 43 16 24
+//12 98 77 51 81 56 27
 
-public class Main {
 
-    private static void function(SLL<Integer> lista) {
-        SLLNode<Integer> iterator = lista.getFirst();
-        int new_number = 0;
+//Input
+//62 19 72
+//8 96 22
+//58 26 71
+//75 51 25
+//32 79 86
+//94 54 10
+//22 34 16
+//38 27 47
+//39 55 26
+//78 80 77
+//99 77 89
+//40 32 46
+//Output
+//62 8 58 75 94
+//22 38 39 32 78 99 40
 
-        while (iterator != null) {
-            if (iterator.element == -1) {
-                lista.insertBefore(new_number, iterator);
-                lista.delete(iterator);
+public class Heroes {
+    public static void startHeroesGame(SLL<Card> firstFriendCards, SLL<Card> secondFriendCards) {
+        SLLNode<Card> bestCard = firstFriendCards.getFirst();
+        SLLNode<Card> firstFriendNode = firstFriendCards.getFirst().succ;
 
-                new_number = 0;
-                iterator = iterator.succ;
-                continue;
+        while (firstFriendNode != null) {
+            if (bestCard.element.damage() < firstFriendNode.element.damage()) {
+                bestCard = firstFriendNode;
             }
 
-            new_number = new_number * 10 + iterator.element;
-            lista.delete(iterator);
-            iterator = iterator.succ;
-
-//            if (iterator == null) { // reshenie na Filip
-//                lista.insertLast(new_number);
-//            }
+            firstFriendNode = firstFriendNode.succ;
         }
 
-        if (new_number != 0) { // reshenie na Irena
-            lista.insertLast(new_number);
+        int n = secondFriendCards.length();
+        SLLNode<Card> secondFriendNode = secondFriendCards.getFirst();
+
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            secondFriendNode = secondFriendNode.succ;
         }
 
-        System.out.println(lista);
+//        int k=(secondFriendCards.length()+1)/2; --> reshenie na Irena
+//        while(k!=0){
+//            second=second.succ;
+//            k--;
+//        }
+
+        secondFriendCards.insertBefore(bestCard.element, secondFriendNode);
+        firstFriendCards.delete(bestCard);
+
+//        int n = secondFriendCards.length(); --> ova e za diskusijata na chas, vo zavisnost od nivnite output-i
+//        if (n % 2 == 0) {
+//            n = (secondFriendCards.length() + 1) / 2;
+//        } else {
+//            n = secondFriendCards.length() / 2;
+//            // vo zavisnost od nivnite output-i ako vo neparna lista go stavaat elementot
+//            // pred sredishniot element togash reshenieto e so ovaa dolzhina
+//        }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        SLL<Card> firstFriendCards = new SLL<Card>();
+        SLL<Card> secondFriendCards = new SLL<Card>();
 
-        int n = sc.nextInt();
-
-        SLL<Integer> lista = new SLL<>();
-
-        for (int i = 0; i < n; i++) {
-            lista.insertLast(sc.nextInt());
+        for (int i = 0; i < 6; i++) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("\\s+");
+            firstFriendCards.insertLast(
+                    new Card(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
         }
 
-        function(lista);
+        for (int i = 0; i < 6; i++) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("\\s+");
+            secondFriendCards.insertLast(
+                    new Card(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+        }
+
+        startHeroesGame(firstFriendCards, secondFriendCards);
+        System.out.println(firstFriendCards.toString());
+        System.out.println(secondFriendCards.toString());
     }
-
-
 }
